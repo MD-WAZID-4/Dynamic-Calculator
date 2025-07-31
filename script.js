89782393110216c41f8575eb68d8d3bbff299464
MD-WAZID-4 <mdwazid9348@gmail.com>
2025-07-31 09:18:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 let string="";
-let string2="";
-let string3="";
-
 let buttons=document.querySelectorAll('.btn');
 
 Array.from(buttons).forEach( (button)=>{
@@ -13,35 +10,11 @@ Array.from(buttons).forEach( (button)=>{
     }
 
     else if(e.target.innerHTML=='C'){
-        string3=string;
+        
         string="";
         document.querySelector('input').value=string;
 
 
-    }
-
-    else  if(e.target.innerHTML=='M+'){
-        string3=string;
-        let store=string3
-        string2=string2+store;
-
-    }
-
-    else  if(e.target.innerHTML=='M-'){
-        let store=string3;
-        string2=string2-store;
-
-    }
-
-    else  if(e.target.innerHTML=='MC'){
-        
-        string2="";
-
-    }
-    else  if(e.target.innerHTML=='MR'){
-        let string4=eval(string2);
-        document.querySelector('input').value=string4;
-       
     }
 
 
