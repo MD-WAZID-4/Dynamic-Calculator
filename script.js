0a4e98d7e12073643748f708501dd5f2d2984924
MD-WAZID-4 <mdwazid9348@gmail.com>
2025-07-31 17:32:30 +0530

    submit

--- script.js
+++ script.js
@@ -1,70 +1,43 @@
-let string="";
-<<<<<<< HEAD
-let string2="";
+let string = "";
+let string2 = "";
+let buttons = document.querySelectorAll('.btn');
 
-=======
->>>>>>> 89782393110216c41f8575eb68d8d3bbff299464
-let buttons=document.querySelectorAll('.btn');
-
-Array.from(buttons).forEach( (button)=>{
-    button.addEventListener('click' , (e)=>{
-    if(e.target.innerHTML=='='){
-        string=eval(string);
-        document.querySelector('input').value=string;
-
-    }
-
-    else if(e.target.innerHTML=='C'){
-<<<<<<< HEAD
-=======
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        } 
         
->>>>>>> 89782393110216c41f8575eb68d8d3bbff299464
-        string="";
-        document.querySelector('input').value=string;
-
-
-    }
-
-<<<<<<< HEAD
-    else  if(e.target.innerHTML=='M+'){
-
-        string2=Number(string2)+Number(document.querySelector('input').value);
-       
-    }
-
-    else  if(e.target.innerHTML=='M-'){
-        if(string2==""){
-            string2=document.querySelector('input').value;
-
+        else if (e.target.innerHTML == 'C') {
+            string = "";
+            document.querySelector('input').value = string;
+        } 
+        
+        else if (e.target.innerHTML == 'M+') {
+            string2 = Number(string2) + Number(document.querySelector('input').value);
+        } 
+        
+        else if (e.target.innerHTML == 'M-') {
+            if (string2 == "") {
+                string2 = document.querySelector('input').value;
+            }
+            
+            else {
+                string2 = Number(string2) - Number(document.querySelector('input').value);
+            }
         }
-        else{
-            string2=Number(string2)-Number(document.querySelector('input').value);
+         else if (e.target.innerHTML == 'MC') {
+            string2 = "";
         }
         
-    }
-
-    else  if(e.target.innerHTML=='MC'){
-        string2="";
+        else if (e.target.innerHTML == 'MR') {
+            document.querySelector('input').value = string2;
+        } 
         
-      
-    }
-    else  if(e.target.innerHTML=='MR'){
-       
-
-        document.querySelector('input').value=string2;
-     
-       
-    }
-
-=======
->>>>>>> 89782393110216c41f8575eb68d8d3bbff299464
-
-    else{
-        string=string+(e.target.innerHTML);
-        document.querySelector('input').value=string;
-    }
-
-    })
-
-})
-
+        else {
+            string = string + (e.target.innerHTML);
+            document.querySelector('input').value = string;
+        }
+    });
+});
